f8178d20f4f553b273366d018d3afc9a0c978efa
zhongpuwu <418743540@qq.com>
2019-05-31 14:44:50 +0800

    Create water_warmer.py

---
+++ water_warmer.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+l2 = []
+
+def SanRe(C):
+
+
+name = ['Year', 'YearlyIncome1', 'YearlyIncome2']
+
+test = pd.DataFrame(columns=name, data=l2)
+test.to_csv('D:/2/warmer1.csv')
